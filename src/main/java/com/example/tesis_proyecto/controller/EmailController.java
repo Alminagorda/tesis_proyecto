27fc17d289eb41319489e3b7f6abb360522d93c6
Alminagorda <134451182+Alminagorda@users.noreply.github.com>
2026-05-08 22:23:07 -0500

    actualización email

--- src/main/java/com/example/tesis_proyecto/controller/EmailController.java
+++ src/main/java/com/example/tesis_proyecto/controller/EmailController.java
@@ -1,13 +1,10 @@
 package com.example.tesis_proyecto.controller;
 
 import com.example.tesis_proyecto.model.NotificationPreference;
+import com.example.tesis_proyecto.service.EmailService;
 import com.example.tesis_proyecto.service.NotificationPreferenceService;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -20,9 +17,8 @@ import java.util.UUID;
 public class EmailController {
 
     private final NotificationPreferenceService preferenceService;
+    private final EmailService emailService;
 
-    @Autowired
-    private JavaMailSender mailSender;
     @GetMapping("/preferences/{userId}")
     public ResponseEntity<NotificationPreference> getPreferences(
             @PathVariable UUID userId) {
@@ -46,14 +42,7 @@ public class EmailController {
     @PostMapping("/test")
     public ResponseEntity<String> testEmail() {
         try {
-            MimeMessage message =
-                    mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            helper.setFrom("gala.alminagorda@gmail.com", "Sistema Antamina");
-            helper.setTo("60659997@pronabec.edu.pe"); // te lo mandas a ti mismo
-            helper.setSubject("🧪 Prueba de email — Sistema Antamina");
-            helper.setText("<h2>El email funciona correctamente ✅</h2>", true);
-            mailSender.send(message);
+            emailService.sendOtpEmail("60659997@pronabec.edu.pe", "123456");
             return ResponseEntity.ok("Email enviado correctamente");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error: " + e.getMessage());
